d67103b23030f3b085bfc968d52e9824360aa30d
yexue <1453646819@qq.com>
2021-12-14 14:53:52 +0800

    "所以再见吧,再见吧"

--- src/main/java/com/example/gitcan/GitcanApplication.java
+++ src/main/java/com/example/gitcan/GitcanApplication.java
@@ -9,6 +9,7 @@ public class GitcanApplication {
     public static void main(String[] args) {
         SpringApplication.run(GitcanApplication.class, args);
         System.out.println("那些回不去的清早,又失了眠");
+        System.out.println("谁会怀念那个夏天");
     }
 
 }
