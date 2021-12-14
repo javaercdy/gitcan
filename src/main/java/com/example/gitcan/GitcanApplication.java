85335cfd5cdcbacd82def4075ad11856f6ddc03f
yexue <1453646819@qq.com>
2021-12-14 14:45:43 +0800

    "谁会怀念那个夏天"

---
+++ src/main/java/com/example/gitcan/GitcanApplication.java
@@ -0,0 +1,14 @@
+package com.example.gitcan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitcanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitcanApplication.class, args);
+        System.out.println("那些回不去的清早,又失了眠");
+    }
+
+}
